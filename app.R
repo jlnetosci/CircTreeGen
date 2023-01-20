5a5c3ae25a9993c7884c4f34f7ba085773b50fdd
João Neto <jlnetosci@gmail.com>
2023-01-20 22:24:23 +0000

    Create UI and server for app. Update get_segments().

--- app.R
+++ app.R
@@ -1,31 +1,81 @@
+library(shiny)
 library(ggplot2)
 library(ggforce)
 
-get_circles <- function(n) {
-  df_circles <- data.frame(x = rep(0,n), y = rep(0,n), r = rep(1:n))
-  return(df_circles)
-}
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("n", "Select number of circles:", choices = 2:20, selected = 5),
+      downloadButton("downloadPDF", "Save as PDF"),
+      downloadButton("downloadSVG", "Save as SVG"),
+      selectInput("size", "Select plot size:",
+                  choices = c("Square" = "square", "A4" = "a4", "Letter" = "letter"))
+      
+    ),
+    mainPanel(
+      tags$div(style = "display: flex; align-items: center; justify-content: center; height: 600px;", plotOutput("plot", width = "600px", height = "600px"))
+    )
+  )
+)
 
-get_segments <- function(n) {
-  df_circles <- get_circles(n)
-  segments_list <- list()
-  for (i in 2:n) {
-    if (i==2) {
-      theta <- seq(0, 2*pi, length.out = 3)[-1]
-    } else {
-      theta <- seq(0, 2*pi, length.out = 2^(i-1)+1)[-1]
+server <- function(input, output) {
+  
+  get_segments <- function(n) {
+    segments_list <- list()
+    for (i in 2:n) {
+      if (i==2) {
+        theta <- seq(0, 2*pi, length.out = 3)[-1]
+      } else {
+        theta <- seq(0, 2*pi, length.out = 2^(i-1)+1)[-1]
+      }
+      df_segment <- data.frame(x = (i-1)*cos(theta), y = (i-1)*sin(theta), xend = i*cos(theta), yend = i*sin(theta), r = rep(i,2^(i-2)))
+      segments_list[[i-1]] <- df_segment
     }
-    df_segment <- data.frame(x = (df_circles$r[i-1])*cos(theta), y = (df_circles$r[i-1])*sin(theta), xend = (df_circles$r[i])*cos(theta), yend = (df_circles$r[i])*sin(theta), r = rep(i,2^(i-2)))
-    segments_list[[i-1]] <- df_segment
+    return(segments_list)
   }
-  return(segments_list)
+  
+  output$plot <- renderPlot({
+    n <- input$n
+    segments_list <- get_segments(n)
+    df_circles <- data.frame(x = rep(0,n), y = rep(0,n), r = 1:n)
+    df_segments <- do.call(rbind,segments_list)
+    ggplot() +
+      geom_circle(data = df_circles, aes(x0 = x, y0 = y, r = r), color = "black") +
+      geom_segment(data = df_segments, aes(x = x, y = y, xend = xend, yend = yend)) +
+      coord_equal() +
+      theme_void()
+    
+  })
+  
+  output$downloadPDF <- downloadHandler(
+    filename = "plot.pdf",
+    content = function(file) {
+      if (input$size == "square") {
+        ggsave(file, plot = last_plot(), width = 20, height = 20, units = "cm", device = "pdf")
+      }
+      if (input$size == "a4") {
+        ggsave(file, plot = last_plot(), width = 21, height = 29.7, units = "cm", device = "pdf")
+      }
+      if (input$size == "letter") {
+        ggsave(file, plot = last_plot(), width = 21.6, height = 27.9, units = "cm", device = "pdf")
+      }
+    }
+  )
+  
+  output$downloadSVG <- downloadHandler(
+    filename = "plot.svg",
+    content = function(file) {
+      if (input$size == "square") {
+        ggsave(file, plot = last_plot(), width = 20, height = 20, units = "cm", device = "svg")
+      }
+      if (input$size == "a4") {
+        ggsave(file, plot = last_plot(), width = 21, height = 29.7, units = "cm", device = "svg")
+      }
+      if (input$size == "letter") {
+        ggsave(file, plot = last_plot(), width = 21.6, height = 27.9, units = "cm", device = "svg")
+      }
+    }
+  )
 }
 
-df_circles <- get_circles(10)
-segments_list <- get_segments(10)
-df_segments <- do.call(rbind,segments_list)
-
-ggplot() +
-  geom_circle(data = df_circles, aes(x0 = x, y0 = y, r = r), color = "black") +
-  geom_segment(data = df_segments, aes(x = x, y = y, xend = xend, yend = yend)) +
-  theme_void()
+shinyApp(ui, server)
